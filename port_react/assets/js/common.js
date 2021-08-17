286adace9050e1378bfd9e2078d13658fdf94e43
seyoungjoy <diddpwl80@gmail.com>
2021-08-17 11:50:09 +0900

    2021.08.17

--- port_react/assets/js/common.js
+++ port_react/assets/js/common.js
@@ -39,3 +39,28 @@ window.onload = function () {
 //     });a
 // }
 // pageClick();
+
+//반응형 네비
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav');
+    const navLinks = document.querySelectorAll('nav ul li');
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+
+};
+navSlide();
