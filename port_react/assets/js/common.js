320129a923e09823ad92445dfdbe29c41c647df6
seyoungjoy <diddpwl80@gmail.com>
2021-08-19 20:29:43 +0900

    2021.08.19

--- port_react/assets/js/common.js
+++ port_react/assets/js/common.js
@@ -2,7 +2,6 @@ window.onload = function () {
     function start(callback) {
         setTimeout(() => {
         document.querySelector(".loader").classList.remove("loader--active");
-        //callback();
         }, 100);
     };
     start();
@@ -21,24 +20,7 @@ window.onload = function () {
         });
     };
     pageClick();
-    
 };
-
-// function pageClick() {
-//     document.querySelectorAll(".page__click").forEach((elem) => {
-//         elem.addEventListener("click", (e) => {
-//             e.preventDefault();
-//             const dataName = elem.getAttribute('href');
-//             document.querySelector(".animation").classList.add("show");
-//             document.querySelector("#header").classList.add("show");
-//             setTimeout(() => {
-//                 window.location.href =
-//                     "http://fejoy95.dothome.co.kr/port_react/" + dataName;
-//             }, 2000);
-//         });
-//     });a
-// }
-// pageClick();
 
 //반응형 네비
 const navSlide = () => {
